84b70c03c68a1ff2d42a396b68840b0c0fb388b9
ZhanliangAaronWang <145517455+ZhanliangAaronWang@users.noreply.github.com>
2025-09-16 04:51:55 +0800

    Delete Supplementary_Material directory

--- Supplementary_Material/inference/ece_evaluator.py
+++
@@ -1,7 +0,0 @@
-import torch
-def ece_evaluator(TP_count, Total_count, confidence_bins):
-    accuracy = torch.div(TP_count, Total_count)
-    weight = torch.div(Total_count, Total_count.sum())
-    ece = torch.multiply(weight, (accuracy - confidence_bins).abs()).nan_to_num().sum()
-    accuracy_batches = torch.multiply(weight, accuracy).nan_to_num().sum()
-    return ece, accuracy_batches
--- Supplementary_Material/utils/group_dividing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-def get_adaptive_bins(num_bins):
-    """Returns indices for binning an equal number of datapoints per bin."""
-    # if np.size(logits, axis=0) == 0:
-    #     return np.linspace(0, 1, num_bins+1)[:-1]
-    edge_indices = np.linspace(0.0, 1.0, num_bins, endpoint=False)
-    return torch.tensor(edge_indices)
